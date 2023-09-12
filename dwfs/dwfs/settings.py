9095db3af26fdc509b2a08c7bb2df2060ae3bdc8
Riccelli <riccelli@outlook.com>
2023-09-12 10:43:03 -0300

    Base PostgreSQL

--- dwfs/dwfs/settings.py
+++ dwfs/dwfs/settings.py
@@ -74,9 +74,13 @@ WSGI_APPLICATION = "dwfs.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'HOST': 'silly.db.elephantsql.com',
+        'PORT': '5432',
+        'NAME': 'yxqsylpn',
+        'USER': 'yxqsylpn',
+        'PASSWORD': 'bPLorQIhjkG-A144F2JGvKaE0MKCosup',
     }
 }
 
@@ -103,9 +107,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = "en-us"
+LANGUAGE_CODE = "pt-br"
 
-TIME_ZONE = "UTC"
+TIME_ZONE = "America/Fortaleza"
 
 USE_I18N = True
 
